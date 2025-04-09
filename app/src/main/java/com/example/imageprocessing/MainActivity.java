566bf077ce4f2cc9218b26a96f233d4d27a5c16f
Emily Anderson <emilyeganderson@gmail.com>
2025-04-09 18:27:33 -0400

    basic propsal skeleton

--- app/src/main/java/com/example/imageprocessing/MainActivity.java
+++ app/src/main/java/com/example/imageprocessing/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.imageprocessing;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,14 +11,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { Button b1; Button b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        b1 = findViewById(R.id.NoiseMasking);
+        b1.setOnClickListener(
+                new View.OnClickListener(){ @Override
+                public void onClick(View v){
+                    Intent i = new Intent(MainActivity.this,MainActivityNoiseMasking.class);
+                    startActivity(i);}
+            }
+        );
+
+        b2 = findViewById(R.id.HDR);
+        b2.setOnClickListener(
+                new View.OnClickListener(){ @Override
+                public void onClick(View v){ Intent i = new
+                        Intent(MainActivity.this,MainActivityHighDynamicRange.class);
+                    startActivity(i);}
+                }
+        );
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
---
+++ sdk/build/intermediates/cxx/Debug/3q521w14/logs/x86/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\emily\\AppData\\Local\\Android\\ssdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\emily\\AndroidStudioProjects\\ImageProcessing\\sdk\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_NDK=C:\\Users\\emily\\AppData\\Local\\Android\\ssdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\emily\\AppData\\Local\\Android\\ssdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\emily\\AppData\\Local\\Android\\ssdk\\ndk\\27.0.12077973\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\emily\\AppData\\Local\\Android\\ssdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\emily\\AndroidStudioProjects\\ImageProcessing\\sdk\\build\\intermediates\\cxx\\Debug\\3q521w14\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\emily\\AndroidStudioProjects\\ImageProcessing\\sdk\\build\\intermediates\\cxx\\Debug\\3q521w14\\obj\\x86" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\emily\\AndroidStudioProjects\\ImageProcessing\\sdk\\.cxx\\Debug\\3q521w14\\x86" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
