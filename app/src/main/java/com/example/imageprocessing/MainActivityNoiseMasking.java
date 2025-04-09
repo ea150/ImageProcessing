8399ae88b7be2078f3fd1aa0ae7a43b36be4e897
Emily Anderson <emilyeganderson@gmail.com>
2025-04-09 19:13:02 -0400

    Noise Masking Proposal changes

--- app/src/main/java/com/example/imageprocessing/MainActivityNoiseMasking.java
+++ app/src/main/java/com/example/imageprocessing/MainActivityNoiseMasking.java
@@ -1,6 +1,9 @@
 package com.example.imageprocessing;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +11,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivityNoiseMasking extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {Button b1;
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_noise_masking);
+
+        b1 = findViewById(R.id.BackButton);
+        b1.setOnClickListener(
+                new View.OnClickListener(){ @Override
+                public void onClick(View v){
+                    Intent i = new Intent(MainActivityNoiseMasking.this,MainActivity.class);
+                    startActivity(i);}
+                }
+        );
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
