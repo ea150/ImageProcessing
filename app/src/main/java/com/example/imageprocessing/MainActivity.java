d791e65a05aded64a0205e6b6b4bacfbc1cfa509
nicksalvemini <nicksalvemini98@gmail.com>
2025-04-09 19:09:24 -0400

    Main launcher and HDR and some strings

--- app/src/main/java/com/example/imageprocessing/MainActivity.java
+++ app/src/main/java/com/example/imageprocessing/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.imageprocessing;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -11,7 +10,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity { Button b1; Button b2;
+public class MainActivity extends AppCompatActivity {
+
+    Button noiseMaskingButton;
+    Button hdrButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,25 +21,21 @@ public class MainActivity extends AppCompatActivity { Button b1; Button b2;
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        b1 = findViewById(R.id.NoiseMasking);
-        b1.setOnClickListener(
-                new View.OnClickListener(){ @Override
-                public void onClick(View v){
-                    Intent i = new Intent(MainActivity.this,MainActivityNoiseMasking.class);
-                    startActivity(i);}
-            }
+        noiseMaskingButton = findViewById(R.id.NoiseMasking);
+        noiseMaskingButton.setOnClickListener(v -> {
+            Intent i = new Intent(MainActivity.this,MainActivityNoiseMasking.class);
+            startActivity(i);
+        }
         );
 
-        b2 = findViewById(R.id.HDR);
-        b2.setOnClickListener(
-                new View.OnClickListener(){ @Override
-                public void onClick(View v){ Intent i = new
-                        Intent(MainActivity.this,MainActivityHighDynamicRange.class);
-                    startActivity(i);}
-                }
+        hdrButton = findViewById(R.id.HDR);
+        hdrButton.setOnClickListener(v -> {
+            Intent i = new Intent(MainActivity.this,MainActivityHighDynamicRange.class);
+            startActivity(i);
+        }
         );
 
-            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
--- app/src/main/java/com/example/imageprocessing/MainActivityHighDynamicRange.java
+++ app/src/main/java/com/example/imageprocessing/MainActivityHighDynamicRange.java
@@ -1,6 +1,8 @@
 package com.example.imageprocessing;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivityHighDynamicRange extends AppCompatActivity {
 
+    Button backButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_high_dynamic_range);
+
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivityHighDynamicRange.this, MainActivity.class);
+            startActivity(intent);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
