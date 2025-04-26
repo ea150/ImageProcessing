3f36b4c47b10a20b71f5ff9ff86802eeee0e589e
nicksalvemini <nicksalvemini98@gmail.com>
2025-04-26 14:39:17 -0400

    hdr sample

--- app/src/main/java/com/example/imageprocessing/MainActivityHighDynamicRange.java
+++ app/src/main/java/com/example/imageprocessing/MainActivityHighDynamicRange.java
@@ -2,17 +2,23 @@ package com.example.imageprocessing;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivityHighDynamicRange extends AppCompatActivity {
 
-    Button backButton;
+    Button backButton, photoButton, hdrButton;
+    ImageView previewImage;
+    TextView previewLabel, tagline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,24 @@ public class MainActivityHighDynamicRange extends AppCompatActivity {
         backButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivityHighDynamicRange.this, MainActivity.class);
             startActivity(intent);
+        });
+
+        previewImage = findViewById(R.id.previewImage);
+        previewLabel = findViewById(R.id.previewLabel);
+        tagline = findViewById(R.id.tagline);
+
+        photoButton = findViewById(R.id.photoButton);
+        photoButton.setOnClickListener(v -> {
+            // TODO This is where we will use the camera class to take picture
+            previewImage.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.mountain, getTheme()));
+            previewImage.setVisibility(View.VISIBLE);
+            previewLabel.setVisibility(View.INVISIBLE);
+            tagline.setText(R.string.hdr_apply_or_take);
+        });
+
+        hdrButton = findViewById(R.id.hdrButton);
+        hdrButton.setOnClickListener(v -> {
+            // TODO This is where we will apply HDR
         });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
