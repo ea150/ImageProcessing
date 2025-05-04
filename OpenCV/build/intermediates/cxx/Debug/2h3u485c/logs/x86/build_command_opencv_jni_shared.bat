a7e0081ccba457cf302c2d6da1109ffc09bfd78d
Emily Anderson <emilyeganderson@gmail.com>
2025-05-04 13:04:46 -0400

    Inital clean setup of RAW camera Activity plus camera activity layout

---
+++ OpenCV/build/intermediates/cxx/Debug/2h3u485c/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\emily\\AppData\\Local\\Android\\ssdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\emily\\AndroidStudioProjects\\ImageProcessing\\OpenCV\\.cxx\\Debug\\2h3u485c\\x86" ^
+  opencv_jni_shared
